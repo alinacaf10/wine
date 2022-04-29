143f596d88ec59c29d1b6fd86af18456e8bbc00d
alinacaf10 <100665352+alinacaf10@users.noreply.github.com>
2022-04-29 11:22:10 +0400

    Add files via upload

---
+++ comp/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Wine from './comp/Wine/wine';
+function App() {
+  return (
+    <div>
+    <Wine
+    title="Wine"
+    desc="keep at cold place"
+    rating="4.5"
+    like
+    />
+    </div>
+  );
+}
+
+export default App;
